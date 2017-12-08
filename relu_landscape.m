854994ac5f5a368a3f95d0d57c068b7fe4e638b1
morino_hikari <sansmori@gmail.com>
2017-12-08 16:59:07 +0800

    a simulation almost finished

--- relu_landscape.m
+++ relu_landscape.m
@@ -1,6 +1,23 @@
 clear all;
 load data_500;
 
+%% just use X as the input on (NI)^n
 
 %% define the relu-2 net
-relu_2 = @(X, w) max([(X*w)';zeros(1, size(X, 1))]);
+relu_2 = @(X, w) max([dot(X,w) , 0]);
+D = @(X, w) double(dot(X,w) > 0);
+
+
+
+%% the teacher network parameter
+w_star = randn(n, 1);
+
+x = X(randi([1, size(X, 1)]), :);
+
+
+expected_term = D(x, w_star)*x*w_star;
+
+delta_J = @(w) x*D(x, w)*(D(x,w)*x*w - expected_term);
+
+disp(delta_J(randn(n,1)));
+
